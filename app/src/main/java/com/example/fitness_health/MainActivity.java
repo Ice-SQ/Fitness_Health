81054c82431dcccd319713e1503058ed31d7387f
Ice-SQ <asithaindrajith12345@gmail.com>
2023-04-02 12:35:32 +0530

    first

---
+++ app/src/main/java/com/example/fitness_health/MainActivity.java
@@ -0,0 +1,62 @@
+package com.example.fitness_health;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+
+import com.example.fitness_health.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        replaceFragment(new HomeFragment());
+
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+
+            switch (item.getItemId()){
+
+                case R.id.home:
+                    replaceFragment(new HomeFragment());
+                    break;
+                case R.id.Calculator:
+                    replaceFragment(new CalculatorFragment());
+                    break;
+                case R.id.steps:
+                    replaceFragment(new StepsFragment());
+                    break;
+                case R.id.heart:
+                    replaceFragment(new HeartRateFragment());
+                    break;
+                case R.id.exercises:
+                    replaceFragment(new ExercisesFragment());
+                    break;
+
+
+            }
+
+
+            return true;
+        });
+
+    }
+    private void replaceFragment(Fragment fragment){
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout,fragment);
+        fragmentTransaction.commit();
+
+
+
+
+    }
+}
